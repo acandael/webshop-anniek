3aa8b8737655dfea5c4069130b8585048a74fe2c
Anthony Candaele <info@anthonycandaele.com>
2021-12-09 14:24:39 +0100

    add ondernemingsnummer to contact page

--- src/page-components/contact/index.js
+++ src/page-components/contact/index.js
@@ -2,16 +2,22 @@ import React from 'react';
 
 import { H1 } from 'ui';
 import Layout from 'components/layout';
-import {Outer, ContactHeader, H3, HeroSection, HeroText, GoogleMap} from './styles'
+import {
+  Outer,
+  ContactHeader,
+  H3,
+  HeroSection,
+  HeroText,
+  GoogleMap
+} from './styles';
 import Breadcrumb from 'components/breadcrumb';
-import { PhoneIcon, EmailIcon} from '@chakra-ui/icons'
+import { PhoneIcon, EmailIcon } from '@chakra-ui/icons';
 
 export default function ContactPage() {
-  
   return (
     <Layout
-      title='contact'
-      description='Op afspraak. Tijdens behandelingen wordt er niet opgenomen maar laat uw naam en telefoon na dan bel ik u zeker terug.'
+      title="contact"
+      description="Op afspraak. Tijdens behandelingen wordt er niet opgenomen maar laat uw naam en telefoon na dan bel ik u zeker terug."
     >
       <Outer>
         <Breadcrumb path="/Home/contact" />
@@ -20,21 +26,40 @@ export default function ContactPage() {
         </ContactHeader>
         <HeroSection>
           <HeroText>
-            <p><EmailIcon color="brand.900" /><a href="mailto:info@anniek-lambrecht.be">info@anniek-lambrecht.be</a><br />
-            <PhoneIcon color="brand.900" /> <a href="tel:050694165">050 69.41.65</a>
+            <p>
+              <EmailIcon color="brand.900" />
+              <a href="mailto:info@anniek-lambrecht.be">
+                info@anniek-lambrecht.be
+              </a>
+              <br />
+              <PhoneIcon color="brand.900" />{' '}
+              <a href="tel:050694165">050 69.41.65</a>
             </p>
-            <p>L. Dujardinstraat 9, 8370<br />
-Blankenberge</p>
+            <p>
+              L. Dujardinstraat 9, 8370
+              <br />
+              Blankenberge
+            </p>
+            <p>Ondernemingsnummer: BE0536204914</p>
             <H3>Openingsuren</H3>
             <p>
-            Op afspraak. Tijdens behandelingen wordt er niet opgenomen maar laat uw naam en telefoon na dan bel ik u zeker terug.
+              Op afspraak. Tijdens behandelingen wordt er niet opgenomen maar
+              laat uw naam en telefoon na dan bel ik u zeker terug.
             </p>
-            <p><strong>Gesloten op: Zaterdag, Zon- en Feestdagen</strong></p>
+            <p>
+              <strong>Gesloten op: Zaterdag, Zon- en Feestdagen</strong>
+            </p>
           </HeroText>
         </HeroSection>
       </Outer>
       <GoogleMap>
-      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2493.9281476296796!2d3.1223149154920717!3d51.31244623276007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c4aa7945f9742b%3A0xc1a4892e7c8bc7df!2sAnniek+Lambrecht!5e0!3m2!1snl!2sbe!4v1522758563152" width="100%" height="650" frameborder="0" allowfullscreen></iframe>
+        <iframe
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2493.9281476296796!2d3.1223149154920717!3d51.31244623276007!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x47c4aa7945f9742b%3A0xc1a4892e7c8bc7df!2sAnniek+Lambrecht!5e0!3m2!1snl!2sbe!4v1522758563152"
+          width="100%"
+          height="650"
+          frameborder="0"
+          allowfullscreen
+        ></iframe>
       </GoogleMap>
     </Layout>
   );
