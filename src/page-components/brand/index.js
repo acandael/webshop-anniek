c7996b453898804ea3032f1fd50810c56db53785
Anthony Candaele <anthony.candaele@tothepoint.company>
2021-12-30 10:24:22 +0100

    add brochure link

--- src/page-components/brand/index.js
+++ src/page-components/brand/index.js
@@ -3,10 +3,17 @@ import Link from 'next/link';
 import { simplyFetchFromGraph } from 'lib/graph';
 import { Outer, H1 } from 'ui';
 import Layout from 'components/layout';
-import Breadcrumb from 'components/breadcrumb'
+import Breadcrumb from 'components/breadcrumb';
 import Microformat from 'components/microformat';
 import toText from '@crystallize/content-transformer/toText';
-import { List, BrandHeader, Content, ImageWrapper, Img, StyledLink} from './styles';
+import {
+  List,
+  BrandHeader,
+  Content,
+  ImageWrapper,
+  Img,
+  StyledLink
+} from './styles';
 import query from './query';
 
 export async function getData({ asPath, language, preview = null }) {
@@ -18,7 +25,7 @@ export async function getData({ asPath, language, preview = null }) {
       version: preview ? 'draft' : 'published'
     }
   });
-  
+
   return { ...data, preview };
 }
 
@@ -26,12 +33,15 @@ export default function BrandPage({ folder, preview }) {
   const { children, components } = folder;
   const description = components?.find((c) => c.name === 'Beschrijving')
     ?.content?.paragraphs[0].body.json;
-  
-  const images = components?.find((c) => c.type === 'images')
-  const image = images?.content?.images[0]
 
-  const moreinfo = components?.find((c) => c.name === 'Meerinfo')?.content?.text;
-  
+  const images = components?.find((c) => c.type === 'images');
+  const image = images?.content?.images[0];
+
+  const moreinfo = components?.find((c) => c.name === 'Meerinfo')?.content
+    ?.text;
+  const brochure = components?.find((c) => c.name === 'Brochure')?.content
+    ?.text;
+
   return (
     <Layout
       title={folder.name}
@@ -44,23 +54,35 @@ export default function BrandPage({ folder, preview }) {
           <Content>
             <H1>{folder.name}</H1>
             <p>{toText(description)}</p>
-            {moreinfo && <Link href={moreinfo}><StyledLink>Meer info</StyledLink></Link>}
+            {moreinfo && (
+              <Link href={moreinfo}>
+                <StyledLink>Meer info</StyledLink>
+              </Link>
+            )}
+            {brochure && (
+              <Link href={brochure}>
+                <StyledLink>Brochure</StyledLink>
+              </Link>
+            )}
           </Content>
           <ImageWrapper>
-            <Img src={image.url} width={image.width} height={image.height} alt={folder.name} />
+            <Img
+              src={image.url}
+              width={image.width}
+              height={image.height}
+              alt={folder.name}
+            />
           </ImageWrapper>
         </BrandHeader>
-        {
-          children && (
-              <List>
-              {children
-            ?.map((item, i) => (
-              item.type = 'brand',
-                <Microformat item={item} key={i} />
-            ))}
-              </List>
-            )
-          }
+        {children && (
+          <List>
+            {children?.map(
+              (item, i) => (
+                (item.type = 'brand'), (<Microformat item={item} key={i} />)
+              )
+            )}
+          </List>
+        )}
       </Outer>
     </Layout>
   );
