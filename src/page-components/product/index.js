ba7f41127dca57a781f79eb0ff7e6cd912979c12
Anthony Candaele <anthony.candaele@tothepoint.company>
2023-12-01 16:01:38 +0100

    remove gebruiksaanwijzing children check

--- src/page-components/product/index.js
+++ src/page-components/product/index.js
@@ -89,8 +89,8 @@ export default function ProductPage({ product, preview }) {
   let tabs;
 
   if (
-    usage?.content?.paragraphs?.[0]?.body.json?.[0]?.children?.length > 0 ||
-    ingredients?.content?.paragraphs?.[0]?.body.json?.[0]?.children?.length > 0
+    usage?.content?.paragraphs?.[0]?.body.json?.[0]?.children.length > 0 ||
+    ingredients?.content?.paragraphs?.[0]?.body.json?.[0]?.children.length > 0
   ) {
     tabs = (
       <Usage>
